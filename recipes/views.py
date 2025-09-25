a36fd0850f0600c5c9e0b9cdaee91a39ed7898d1
Edno Elias Alves <eelias007@gmail.com>
2025-09-24 22:26:21 -0300

    Remoção de arquivos,

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, "recipes/home.html")
-
-def sobre(request):
-    return HttpResponse("SOBRE")
-
-def contato(request):
-    return HttpResponse("CONTATOS")
